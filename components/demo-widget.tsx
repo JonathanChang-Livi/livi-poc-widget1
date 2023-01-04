cad02645e6506d38ef49a719e28081dada8c0f28
jonathanchang <jonathan.chang@livibank.com>
2023-01-04 10:49:45 +0800

    demo

---
+++ components/demo-widget.tsx
@@ -0,0 +1,23 @@
+import { Card, CardBody } from "livi-poc-core"
+import Image from "next/image"
+
+const DemoWidget = () => {
+    return (
+
+        <Card className="bg-primary">
+            <CardBody>
+                <div className="flex flex-row justify-between items-end">
+                    <div className="flex flex-col">
+                        <Image height={30} width={40} src={'/logo-livi-letter.svg'} alt='' />
+                        <p className="text-secondary text-sm font-semibold mt-4">Account Summary: </p>
+                    </div>
+                    <div className="flex flex-col">
+                        <p className="text-secondary text-xl font-bold">$999,999,999</p>
+                    </div>
+                </div>
+            </CardBody>
+        </Card>
+    )
+}
+
+export default DemoWidget
---
+++ next.config.js
@@ -0,0 +1,36 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  distDir: 'build', // Defined build directory
+  webpack: (config, options) => { // webpack configurations
+    config.plugins.push(
+      new options.webpack.container.ModuleFederationPlugin({
+        name:"widget1",
+        filename: "remoteEntry.js", // remote file name which will used later
+        remoteType: "var",
+        exposes: { // expose all component here.
+          "./demo": "./src/component/demo-widget"
+        },
+        shared: [
+          {
+            react: {
+              eager: true,
+              singleton: true,
+              requiredVersion: false,
+            }
+          },
+          {
+            "react-dom": {
+              eager: true,
+              singleton: true,
+              requiredVersion: false,
+            }
+          },
+        ]
+      })
+    )
+    return config
+  }
+}
+
+module.exports = nextConfig
